514c804bda526fa207efef82a968b7694cea9a71
schelotto <canranxu@gmail.com>
2018-07-29 12:28:41 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 import torch
+import torch.nn.functional as F
 
 from tqdm import tqdm
 from model.VAE import RVAE
@@ -20,7 +21,7 @@ if __name__ == '__main__':
     parser.add_argument('-word_emb', type=str, default='glove_840B',
                         help='Word embedding name. In glove_840B, glove_6B or glove_42B')
     parser.add_argument('-save-file', type=str, default=None, help='File path/name for model to be saved.')
-    parser.add_argument('-log-interval', type=int, default=1000, help='Number of iterations to sample generated sentences')
+    parser.add_argument('-log-interval', type=int, default=100, help='Number of iterations to sample generated sentences')
     parser.add_argument('-train_iter', type=int, default=40000, help='Number of iterations for training')
     args = parser.parse_args()
 
@@ -33,6 +34,11 @@ if __name__ == '__main__':
     args.unk = text_field.vocab.stoi['<unk>']
 
     rvae = RVAE(args)
+
+    kld_start_inc = 5000
+    kld_max = 0.15
+    kld_weight = 0.001
+    kld_inc = (kld_max - kld_weight) / (args.train_iter - kld_start_inc)
 
     ###########################
     ## ASSIGN WORD EMBEDDING ##
@@ -59,6 +65,28 @@ if __name__ == '__main__':
     for it in range(args.train_iter):
         batch = next(train_iter)
         text = batch.text
+
         if torch.cuda.is_available():
             text = text.cuda()
-            print(text)
+
+        output, kld, _ = rvae(text)
+
+        if it > kld_start_inc and kld_weight < kld_max:
+            kld_weight += kld_inc
+
+        ce_loss = F.cross_entropy(output, text, size_average=False)
+        kl_loss = kld_weight * kld
+        loss = ce_loss + kl_loss
+
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+
+        if (it + 1) % args.log_interval == 0:
+            print('Iter {}/{} Recon Loss {:.4f} KL Loss {:.4f}'
+                  .format(it + 1, args.train_iter, ce_loss.data.item(), kld.data.item()))
+
+            z = torch.randn(1, args.z_dim)
+            sampled_indices = rvae.sample_sentence(z)
+            sampled_sentence = list(map(lambda x:text_field.itos[x], sampled_indices))
+            print('Sample: {}'.format(' '.join(sampled_sentence)))
