19a7b36b86fb2498fd2e924eb869f16b3d74dd50
schelotto <canranxu@gmail.com>
2018-07-29 13:30:52 +0800

    update

--- model/VAE.py
+++ model/VAE.py
@@ -148,7 +148,7 @@ class RVAE(nn.Module):
             y = F.softmax(y/temp, dim=-1)
 
             idx = torch.max(y.squeeze(), dim=-1)[1]
-            print(idx.data.item())
+            idx = idx.data.item()
 
             word = torch.LongTensor([int(idx)])
             word = word.cuda() if torch.cuda.is_available() else word
